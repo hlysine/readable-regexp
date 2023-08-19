cc21420618cc7e6142c9b621a36eedce96e0855e
Henry Lin <henry.ys.lin@gmail.com>
2023-08-19 11:18:19 +0800

    Export types for convenience of custom extensions

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 export * from './expression';
-export { Flag } from './types';
+export {
+  Flag,
+  /* These types are exported for the convenience of custom extensions */
+  type RegExpToken,
+  type LiteralFunction,
+  type NumberFunction,
+  type TokenFunction,
+  type MultiTokenFunction,
+  type GroupFunction,
+  type AlternationFunction,
+  type NamedCaptureFunction,
+  type RepeatFunction,
+  type LimitFunction,
+  type CharClassFunction,
+  type IncompleteToken,
+} from './types';
